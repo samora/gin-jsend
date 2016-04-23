417234a710c009f5674686947a34f03b7d5ad3b1
Samora Dake <samoradake@gmail.com>
2016-04-23 01:01:43 +0000

    Remove reliance on net/http status codes

--- http_statuses.go
+++ http_statuses.go
@@ -2,45 +2,44 @@ package jsend
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 // OK - 200 http status
 func OK(c *gin.Context, data interface{}) {
-	Success(c, http.StatusOK, data)
+	Success(c, 200, data)
 }
 
 // Created - 201 http status
 func Created(c *gin.Context, data interface{}) {
-	Success(c, http.StatusCreated, data)
+	Success(c, 201, data)
 }
 
 // BadRequest - 400 http status
 func BadRequest(c *gin.Context, data interface{}) {
-	Fail(c, http.StatusBadRequest, data)
+	Fail(c, 400, data)
 }
 
 // Unauthorized - 401 http status
 func Unauthorized(c *gin.Context, code int64, data interface{}) {
-	Error(c, http.StatusUnauthorized, "Unauthorized", code, data)
+	Error(c, 401, "Unauthorized", code, data)
 }
 
 // Forbidden - 403 http status
 func Forbidden(c *gin.Context, code int64, data interface{}) {
-	Error(c, http.StatusForbidden, "Forbidden", code, data)
+	Error(c, 403, "Forbidden", code, data)
 }
 
 // NotFound - 404 http status
 func NotFound(c *gin.Context, code int64, data interface{}) {
-	Error(c, http.StatusNotFound, "Not Found", code, data)
+	Error(c, 404, "Not found", code, data)
 }
 
 // TooManyRequests - 429 http status
 func TooManyRequests(c *gin.Context, code int64, data interface{}) {
-	Error(c, http.StatusTooManyRequests, "Too Many Requests", code, data)
+	Error(c, 429, "Too many requests", code, data)
 }
 
 // InternalServerError - 500 http status
 func InternalServerError(c *gin.Context, msg string, code int64, data interface{}) {
-	Error(c, http.StatusInternalServerError, msg, code, data)
+	Error(c, 500, msg, code, data)
 }
